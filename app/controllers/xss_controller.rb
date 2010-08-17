e0e67596ba36f1277059b85b0267c133397796e0
Andre Pankratz <andre@webwarelab.com>
2010-08-17 15:25:30 +0200

    Transfer request params to xss call

--- app/controllers/xss_controller.rb
+++ app/controllers/xss_controller.rb
@@ -2,6 +2,9 @@ class XssController < ApplicationController
   unloadable
   
   def load
-    render :path => params[:path], :format => :xss
+    custom_params = params.except(:path, :scope, :controller, :action)
+    path = params[:path]
+    path += "?#{custom_params.to_uri}" if custom_params.any?
+    render :path => path, :format => :xss
   end
 end
