244545575751482b16150203d47a75396453a276
Andre Pankratz <andre@webwarelab.com>
2010-10-01 16:09:09 +0200

    Fixed xss_client handling

--- lib/vidibus/xss/extensions/controller.rb
+++ lib/vidibus/xss/extensions/controller.rb
@@ -15,7 +15,7 @@ module Vidibus
 
         # Set hostname of clients that are allowed to access this resource.
         def xss_clients
-          [request.headers["Origin"]]
+          @xss_clients ||= [request.headers["Origin"]]
         end
 
         protected
@@ -29,11 +29,15 @@ module Vidibus
         def xss_client
           @xss_client ||= begin
             return unless origin = request.headers["Origin"]
+            clients = xss_clients
             unless xss_clients
               raise %(Define a list of xss_clients in your ApplicationController that returns all hosts that are allowed to access your service.\nExample: %w[http://myconsumer.local])
             end
-            xss_clients = [xss_clients] unless xss_clients.is_a?(Array)
-            xss_clients.detect { |c| c == origin }
+            if clients.is_a?(Array)
+              clients.detect { |c| c == origin }
+            elsif clients == origin
+              origin
+            end
           end
         end
 
