ddf8c229659190738b48c1b3e1f2ca14795f51b2
Andre Pankratz <andre@webwarelab.com>
2010-10-01 15:35:05 +0200

    Handle string keys in params

--- app/controllers/xss_controller.rb
+++ app/controllers/xss_controller.rb
@@ -2,7 +2,7 @@ class XssController < ApplicationController
   unloadable
   
   def load
-    custom_params = params.except(:path, :scope, :controller, :action)
+    custom_params = params.symbolize_keys.except(:path, :scope, :controller, :action)
     path = params[:path]
     path += "?#{custom_params.to_uri}" if custom_params.any?
     render :path => path, :format => :xss
