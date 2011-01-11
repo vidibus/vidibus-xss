d741c931e5ecc2e7baf14c5ff9124c29f3f7a419
Andre Pankratz <andre@webwarelab.com>
2011-01-11 01:33:55 +0100

    Applied recommendations of JSLint + Cleanup

--- public/javascripts/vidibus.xss.js
+++ public/javascripts/vidibus.xss.js
@@ -25,7 +25,7 @@ vidibus.xss = {
   },
 
   /**
-   * Detects scope of script block to be executed. 
+   * Detects scope of script block to be executed.
    * Must be called from embedding page.
    */
   detectScope: function() {
@@ -35,7 +35,7 @@ vidibus.xss = {
     $detector.remove();
     return $scope;
   },
-  
+
   /**
    * Usage:
    *   vidibus.xss.embed('<div>Some HTML</div>', $('#scope') [, ".some element"]);
@@ -51,18 +51,18 @@ vidibus.xss = {
     this.setActions($scope);
     this.ready();
   },
-  
+
   /**
    * Calls given path for given scope. If a third parameter is set to true,
    * the location will be loaded, even if it is currently loaded.
-   * 
+   *
    * Usage:
    *   vidibus.xss.get('path', $('#scope') [, true]);
-   * 
+   *
    * If no host is provided, host of scope will be used.
    */
   get: function(path, $scope, reload) {
-    // Escape query parts
+    // escape query parts
     path = path.replace("?", "%3F").replace("&", "%26").replace("=", "%3D");
 
     // remove path prefix
@@ -75,20 +75,20 @@ vidibus.xss = {
         keepCurrentLocation = this.initialized[scopeId] ? 0 : 1,
         location = $.param.fragment(String(document.location), params, keepCurrentLocation),
         reloadScope = {};
-    
+
     this.initialized[scopeId] = true;
     window.location.href = location; // write history
     reloadScope[scopeId] = reload;
     this.loadUrl(location, reloadScope);
   },
-  
+
   /**
    * Redirect to given path while forcing reloading.
    */
   redirect: function(path, $scope) {
-    this.get(path, $scope, true)
+    this.get(path, $scope, true);
   },
-  
+
   /**
    * Handles callback action.
    *
@@ -98,41 +98,41 @@ vidibus.xss = {
    * Accepts actions depending on status:
    *   (redirect) to: Performs redirect to location.
    */
-  callback: function(data, $scope) {    
-    if (data.status == 'redirect') {
+  callback: function(data, $scope) {
+    if (data.status === "redirect") {
       this.redirect(data.to, $scope);
     }
   },
-  
+
   /**
    * Sets host for given scope.
    */
   setHost: function(host, $scope) {
     $scope.attr('data-host', host);
   },
-  
+
   /**
    * Returns host for given scope.
    */
   getHost: function($scope) {
     return $scope.attr('data-host');
   },
-  
+
   /**
    * Turns given path into an absolute url.
    */
   getUrl: function(path, host) {
-    if (path.match(/https?:\/\//)) { return path }
+    if (path.match(/https?:\/\//)) { return path; }
     return host + path;
   },
-  
+
   /**
    * Returns relative path from url.
    */
   getPath: function(url) {
-    return url.replace(/https?:\/\/[^\/]+/,'')
+    return url.replace(/https?:\/\/[^\/]+/,'');
   },
-  
+
   /**
    * Rewrites links to absolute urls.
    */
@@ -144,20 +144,20 @@ vidibus.xss = {
       var href = $(this).attr('href');
       $(this).attr('href', vidibus.xss.getUrl(href, host));
     });
-    
+
     // Rewrite forms
     $('form[action]', $scope).each(function(e) {
       var action = $(this).attr('action');
       $(this).attr('href', vidibus.xss.getUrl(action, host));
     });
   },
-  
+
   /**
    * Set xss actions for interactive elements.
    */
   setActions: function($scope) {
     var host = this.getHost($scope);
-     
+
     // Set action for GET links
     // TODO: Allow links to be flagged as "external"
     $('a[href^='+host+']:not([data-method],[data-remote])', $scope).bind('click.xss', function(e) {
@@ -165,7 +165,7 @@ vidibus.xss = {
       vidibus.xss.get(href, $scope);
       e.preventDefault();
     });
-    
+
     // Set action non-GET links
     // TODO: Remove bindings from links that match current host only: a[data-method][href^='+host+']:not([data-remote])
     $('a[data-method]:not([data-remote])').die('click').unbind('click'); // remove bindings
@@ -176,9 +176,9 @@ vidibus.xss = {
       $(this).callAjax(url);
       e.preventDefault();
     });
-    
+
     // Set form action
-    $('form[action]').die('submit').unbind('submit') // remove bindings
+    $('form[action]').die('submit').unbind('submit'); // remove bindings
     $('form[action][href^='+host+']', $scope).submit(function(e) {
       var $form = $(this),
           path = $form.attr('action');
@@ -187,7 +187,7 @@ vidibus.xss = {
       return false;
     });
   },
-  
+
   /**
    * Modifies paths within given html string.
    * This method must be called before embedding html snippet into the page
@@ -195,13 +195,17 @@ vidibus.xss = {
    */
   transformPaths: function(html, $scope) {
     var match, url;
-    while (match = html.match(/src="((?!http)[^"]+)"/)) {
+    while (true) {
+      match = html.match(/src="((?!http)[^"]+)"/);
+      if (!match) {
+        break;
+      }
       url = vidibus.xss.buildUrl(match[1], $scope);
-      html = html.replace(match[0], 'src="'+url+'"')
+      html = html.replace(match[0], 'src="'+url+'"');
     }
     return html;
   },
-  
+
   /**
    * Load XSS sources from given url.
    * If url is empty, the current location will be used.
@@ -209,24 +213,26 @@ vidibus.xss = {
   loadUrl: function(url, reload) {
     var scope, $scope, path, loaded, params = $.deparam.fragment();
     for(scope in params) {
-      path = params[scope];
+      if (params.hasOwnProperty(scope)) {
+        path = params[scope];
 
-      // don't reload locations that have already been loaded
-      loaded = this.loadedUrls[scope];
-      if((!reload || !reload[scope]) && loaded && loaded == path) {
-        continue;
-      }
+        // don't reload locations that have already been loaded
+        loaded = this.loadedUrls[scope];
+        if((!reload || !reload[scope]) && loaded && loaded === path) {
+          continue;
+        }
 
-      $scope = $('#'+scope);
-      if($scope[0] == undefined) {
-        console.log('Scope not found: '+scope);
-      } else {
-        this.loadedUrls[scope] = path;
-        this.loadData(path, $scope);
+        $scope = $('#'+scope);
+        if($scope[0] === undefined) {
+          console.log('Scope not found: '+scope);
+        } else {
+          this.loadedUrls[scope] = path;
+          this.loadData(path, $scope);
+        }
       }
     }
   },
-  
+
   /**
    * Load relative path into given scope.
    * Transforms scope host and path into a XSS location.
@@ -240,7 +246,7 @@ vidibus.xss = {
       type: 'GET'
     });
   },
-  
+
   /**
    * Tranforms local paths to absolute ones.
    */
@@ -251,7 +257,8 @@ vidibus.xss = {
     if(params) {
       params = params.split("&");
     } else {
-      params = []
+      params = [];
+    }
 
     // prepend path prefix
     if (typeof xssPathPrefix !== "undefined" && xssPathPrefix && !path.match(xssPathPrefix)) {
@@ -260,22 +267,22 @@ vidibus.xss = {
 
     var host = this.getHost($scope),
       scope = $scope.attr('id'),
-      url = this.getUrl(path, host)
+      url = this.getUrl(path, host);
 
-    if (!url.match(/\.[a-z]+((\?|\#).*)?$/)) url += '.xss';
-    if (url.indexOf('.xss') > -1 && params.toString().indexOf('scope='+scope) == -1) {
+    if (!url.match(/\.[a-z]+((\?|\#).*)?$/)) { url += '.xss'; }
+    if (url.indexOf('.xss') > -1 && params.toString().indexOf('scope='+scope) === -1) {
       params.push('scope='+scope);
     }
-    
+
     // add cache buster
     if (uncached) {
       var d = new Date();
       params.push(d.getTime());
     }
-    
+
     // append params
     if (params.length) {
-      if (url.indexOf('?') == -1) url += '?'
+      if (url.indexOf('?') === -1) { url += '?'; }
       url += params.join('&');
     }
 
@@ -285,13 +292,13 @@ vidibus.xss = {
 
 /**
  * Detect changes to document's location.
- * 
+ *
  */
 $(function($){
-  
+
   // Detect changes of document.location and trigger loading.
   $(window).bind('hashchange', function(e) {
-    if (vidibus.loader.complete) {
+    if (xssLoader.complete) {
       vidibus.xss.loadUrl();
     }
     return false;
@@ -304,11 +311,10 @@ $(function($){
 });
 
 /**
- * Implement ajax handler. 
- * This is the default handler provided in rails.js extended to accept delete method.
+ * Extend XHR
  */
 $(function($) {
-  
+
   /**
    * Extend xhr object to send credentials and force XMLHttpRequest.
    */
@@ -317,17 +323,17 @@ $(function($) {
     try {
       xhr.withCredentials = "true";
     } catch(e) {
-      alert('Cannot set xhr with credentials:\n'+e)
+      alert('Cannot set xhr with credentials:\n'+e);
     }
   };
-  
+
   /**
    * Extends xhr on beforeSend by binding to Rails' ajax:loading event.
    */
   $("body").bind('ajax:loading', function(e, xhr) {
     extendXhr(xhr);
   });
-  
+
   /**
    * Try to send xhr request withCredentials.
    * Unfortunately, this has to be set after the connection has been opened.
